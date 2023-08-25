238fc6062fc39a91a278970d462e3e66172f0f4f
Bagi Hartawan <hartawanbagie@gmail.com>
2023-08-25 17:51:52 +0800

    initial commit

---
+++ src/main/kotlin/com/bagih/belajarspringdbrelationship/BelajarSpringDbRelationshipApplication.kt
@@ -0,0 +1,11 @@
+package com.bagih.belajarspringdbrelationship
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BelajarSpringDbRelationshipApplication
+
+fun main(args: Array<String>) {
+	runApplication<BelajarSpringDbRelationshipApplication>(*args)
+}
---
+++ src/test/kotlin/com/bagih/belajarspringdbrelationship/BelajarSpringDbRelationshipApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bagih.belajarspringdbrelationship
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BelajarSpringDbRelationshipApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
